c7b70eec6e4095b89e229217a8ad672e2ad6d4df
charles <yinchao.guo@hpe.com>
2025-03-30 08:51:37 -0700

    add header

--- pages/app_admin.py
+++ pages/app_admin.py
@@ -248,9 +248,16 @@ def main():
             print("URL returned")
             print(urls)
             
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Connection": "keep-alive"
+            }         
 
             loader = WebBaseLoader(
                     web_path = list(urls),
+                    header_template=headers,
                     continue_on_failure = True,
                     show_progress = True)
             all_texts = [doc.page_content for doc in loader.load()]
